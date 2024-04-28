45d2432bd6e26dab39cbe3b58e0a89d36668ab64
QuocViet <vietnguyen142002@gmail.com>
2024-04-28 17:14:07 +0700

    fix deploy

--- src/_actions/user.ts
+++ src/_actions/user.ts
@@ -1,16 +1,16 @@
 import { User as UserModel } from '@/models/User';
-import { connect } from '@/lib/mongodb';
+import dbConnect from '@/lib/mongodb';
 
 export const User = {
     login: async (username: string, password: string) => {
-        await connect();
+        await dbConnect();
 
         const user = await UserModel.findOne({ username, password }).exec();
 
         return user;
     },
     register: async (username: string, email: string, password: string) => {
-        await connect();
+        await dbConnect();
         const user = new UserModel({
             username,
             email,
