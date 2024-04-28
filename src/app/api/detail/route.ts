36fd432308dd4bb8f651b94c834806408b3a2065
QuocViet <vietnguyen142002@gmail.com>
2024-04-28 20:22:44 +0700

    fix deploy

--- src/app/api/detail/route.ts
+++ src/app/api/detail/route.ts
@@ -6,6 +6,11 @@ export async function GET(
         params: { id: string; [key: string]: string | string[] | undefined };
     }
 ) {
+    const id = context.params.id;
+
+    if (!id) {
+        return Response.json({ message: 'Not found' }, { status: 404 });
+    }
     const data = await PostAction.getPostById(context.params.id);
     return Response.json({ data });
 }
